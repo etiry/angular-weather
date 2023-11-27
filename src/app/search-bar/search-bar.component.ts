f89d1246c026dd3d16f6190a3c3c585819ae2a41
etiry <7997357+etiry@users.noreply.github.com>
2023-11-27 12:42:22 -0500

    Update search bar and add styling

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -1,25 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule, NgFor } from '@angular/common';
-import { Observable, Subject, debounceTime, distinctUntilChanged, switchMap } from 'rxjs';
+import { FormsModule, NgForm } from '@angular/forms';
 import { WeatherService } from '../services/weather.service';
 import { Location } from '../location';
 
 @Component({
   selector: 'app-search-bar',
   standalone: true,
-  imports: [CommonModule, NgFor],
+  imports: [CommonModule, NgFor, FormsModule],
   templateUrl: './search-bar.component.html',
   styleUrl: './search-bar.component.css'
 })
 export class SearchBarComponent {
+[x: string]: any;
   constructor(private weatherService: WeatherService) {}
 
-  searchResults$!: Location[];
+  search = '';
 
-  onSubmit(term: string) {
+  searchResults!: Location[];
+
+  onSubmit(term: string, form: NgForm) {
+    form.reset();
     return this.weatherService.searchLocations(term)
       .subscribe((results) => {
-        this.searchResults$ = results.map((result: { name: string; state: string; lat: number; lon: number; }) => ({
+        this.searchResults = results.map((result: Location) => ({
           name: result.name,
           state: result.state,
           lat: result.lat,
